97687e63e1a71a36c703a0a323a52c08d6eaa8f8
Manoj Kumar Panda <pandamanoj872@gmail.com>
2024-05-05 16:47:03 +0530

    Update Q162.java

--- Medium/162 - Find peak Ele/Q162.java
+++ Medium/162 - Find peak Ele/Q162.java
@@ -1,1 +1,21 @@
+public class Q162 {
+    static  int peakEle(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        while (start<end){
+            int middle=start+(end-start)/2;
+            if(arr[middle]>arr[middle+1]){
+                end=middle;
+            }
+            else {
+                start=middle+1;
+            }
+        }
+        return start;
+    }
+    public static void main(String[] args) {
+int[] arr={0,1,2,6,8,10,8,6,4,2,1};
+        System.out.println(peakEle(arr));
+    }
+}
 
