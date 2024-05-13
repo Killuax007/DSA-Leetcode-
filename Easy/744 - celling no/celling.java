a20b1307705ab63130153831e2136105210536de
Rakesh Kumar parida <paridarakeshkumar30@gmail.com>
2024-05-13 20:13:38 +0530

    Update celling.java

--- Easy/744 - celling no/celling.java
+++ Easy/744 - celling no/celling.java
@@ -1,1 +1,18 @@
 
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int str = 0;
+        int end = letters.length - 1;
+        while (str <= end) {
+            int mid = str + (end - str) / 2;
+
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else  {
+                str = mid + 1;
+            }
+            }
+    return letters[str%letters.length];
+        
+    }
+}
